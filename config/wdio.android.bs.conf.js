d0fd8e70bf1ba5ea10417aad56ed3957b2f184b6
calinepatriota <calinepatriota@gmail.com>
2022-04-02 11:59:07 -0300

    Ajustes config

--- config/wdio.android.bs.conf.js
+++ config/wdio.android.bs.conf.js
@@ -12,7 +12,7 @@ config.key = process.env.BROWSERSTACK_ACCESS_KEY;
 // ============
 config.specs =[
        // './test/specs/android/delete-note-screen*.js'
-       'test/specs/**',
+       'test/specs/android/**',
  ];
 
 // ============
